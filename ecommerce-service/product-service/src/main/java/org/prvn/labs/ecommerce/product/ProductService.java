9a2aaae5cebb1837aa9dadf1ccd87ce85c54c747
Praveen Are <arewar.praveen@gmail.com>
2025-05-21 17:07:35 +0900

    Initial Commit

---
+++ ecommerce-service/product-service/src/main/java/org/prvn/labs/ecommerce/product/ProductService.java
@@ -0,0 +1,11 @@
+package org.prvn.labs.ecommerce.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductService{
+    public static void main(String[] args) {
+        SpringApplication.run(ProductService.class,args);
+    }
+}
